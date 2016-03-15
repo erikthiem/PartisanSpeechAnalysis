e6f13ca1e3c6b25464414462484d107ae635f5f6
Erik Thiem <erikthiem@gmail.com>
2016-03-15 17:15:09 -0400

    Downloaded all speeches and created speeches.db dqlite file from them

--- data/DownloadData.py
+++ data/DownloadData.py
@@ -36,6 +36,7 @@ class Candidate():
             r = urllib.urlopen(link).read()
             speech_text = (BeautifulSoup(r).find_all("span", attrs={'class': "displaytext"})[0].getText()).encode("ascii", "ignore")
             self.speeches.append(speech_text)
+            print("Downloaded speech #{0} from {1}".format(len(self.speeches), self.name))
 
 # Open file
 filename = "links.txt"
@@ -57,13 +58,20 @@ for i in range(len(names)):
 # Get each candidate's speeches and store their links to an array in the Candidate object
 # TODO: Set to download all candidates speeches, not just candidates[40] (this is in here right now for debugging/developing because it takes
 #       a while to download all of the speeches
-candidates[40].downloadSpeeches()
+for c in candidates:
+    c.downloadSpeeches()
+    print("Downloaded speeches from {0}".format(c.name))
 
 # TODO: Extend this to all candidates and all of their speeches
 # TODO: Add some checking so an existing table isn't accidentally overwritten.
 conn = sqlite3.connect('speeches.db')
 c = conn.cursor()
 c.execute('''CREATE TABLE speeches (name text, party text, year integer, speech text)''')
-c.execute("INSERT INTO speeches VALUES (?, ?, ?, ?)", (candidates[40].name, candidates[40].party, candidates[40].year, candidates[40].speeches[0]))
+
+for candidate in candidates:
+    for speech in candidate.speeches:
+        c.execute("INSERT INTO speeches VALUES (?, ?, ?, ?)", (candidate.name, candidate.party, candidate.year, speech))
+    print("Saved speeches to database from {0}".format(candidate.name))
+
 conn.commit()
 conn.close()
