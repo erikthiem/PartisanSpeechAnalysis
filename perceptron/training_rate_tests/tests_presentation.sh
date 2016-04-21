89657232cc7b863f1fa53c7c1596e524bcf20116
Erik Thiem <erikthiem@gmail.com>
2016-04-21 16:26:01 -0400

    ran new tests for the presentation

---
+++ perceptron/training_rate_tests/tests_presentation.sh
@@ -0,0 +1,6 @@
+time python ../perceptron.py ../../data/train.db ../../data/test.db 1 0.9
+time python ../perceptron.py ../../data/train.db ../../data/test.db 5 0.9
+time python ../perceptron.py ../../data/train.db ../../data/test.db 10 0.9
+time python ../perceptron.py ../../data/train.db ../../data/test.db 20 0.9
+time python ../perceptron.py ../../data/train.db ../../data/test.db 50 0.9
+time python ../perceptron.py ../../data/train.db ../../data/test.db 100 0.9
