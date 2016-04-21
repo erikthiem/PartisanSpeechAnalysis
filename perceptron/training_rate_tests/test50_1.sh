0f4e82be932cc82f1b7a3b49975ad9b8aec4b3f5
Erik Thiem <erikthiem@gmail.com>
2016-04-20 22:38:32 -0400

    added 50-iteration tests to the training_rate_determination file

---
+++ perceptron/training_rate_tests/test50_1.sh
@@ -0,0 +1,5 @@
+python ../perceptron.py ../../data/train.db ../../data/test.db 50 0.1
+python ../perceptron.py ../../data/train.db ../../data/test.db 50 0.2
+python ../perceptron.py ../../data/train.db ../../data/test.db 50 0.3
+python ../perceptron.py ../../data/train.db ../../data/test.db 50 0.4
+python ../perceptron.py ../../data/train.db ../../data/test.db 50 0.5
---
+++ perceptron/training_rate_tests/test50_2.sh
@@ -0,0 +1,5 @@
+python ../perceptron.py ../../data/train.db ../../data/test.db 50 0.6 
+python ../perceptron.py ../../data/train.db ../../data/test.db 50 0.7 
+python ../perceptron.py ../../data/train.db ../../data/test.db 50 0.8 
+python ../perceptron.py ../../data/train.db ../../data/test.db 50 0.9 
+python ../perceptron.py ../../data/train.db ../../data/test.db 50 1.0
